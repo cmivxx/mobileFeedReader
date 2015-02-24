175f96d2d8b918a3752d39cd55b26cfa23e5293f
Chris Miller <chris@intellagentbenefits.com>
2015-02-24 00:11:48 -0600

    initial development

---
+++ Feeds/www/data/data.js
@@ -0,0 +1,142 @@
+var feedData = {
+	feedData: [
+	    {
+	        "idnum": "1",
+	        "name": "SlickDeals.net",
+	        "url": "http://www.slickdeals.net",
+	        "rss": "http://feeds.feedburner.com/SlickdealsnetFP"
+	    },
+	    {
+	        "idnum": "2",
+	        "name": "TMZ",
+	        "url": "http://www.tmz.com/",
+	        "rss": "http://www.tmz.com/rss.xml"
+	    },
+	    {
+	        "idnum": "3",
+	        "name": "MajorNelson",
+	        "url": "http://www.majornelson.com",
+	        "rss": "http://feeds.feedburner.com/MajorNelson"
+	    },
+	    {
+	        "idnum": "19",
+	        "name": "The Gadgeteer",
+	        "url": "http://the-gadgeteer.com/",
+	        "rss": "http://feedproxy.google.com/TheGadgeteer"
+	    },
+	    {
+	        "idnum": "5",
+	        "name": "Lifehacker",
+	        "url": "http://www.lifehacker.com",
+	        "rss": "http://feeds.gawker.com/lifehacker/full"
+	    },
+	    {
+	        "idnum": "6",
+	        "name": "Gizmodo",
+	        "url": "http://www.gizmodo.com",
+	        "rss": "http://feeds.gawker.com/Gizmodo/full"
+	    },
+	    {
+	        "idnum": "7",
+	        "name": "EnGadget",
+	        "url": "http://www.engadget.com",
+	        "rss": "http://www.engadget.com/rss.xml"
+	    },
+	    {
+	        "idnum": "8",
+	        "name": "TUAW",
+	        "url": "http://www.tuaw.com",
+	        "rss": "http://www.tuaw.com/rss.xml"
+	    },
+	    {
+	        "idnum": "9",
+	        "name": "Redmond Pie",
+	        "url": "http://www.redmondpie.com",
+	        "rss": "http://www.redmondpie.com/feed"
+	    },
+	    {
+	        "idnum": "10",
+	        "name": "Joystiq",
+	        "url": "http://www.joystiq.com",
+	        "rss": "http://www.joystiq.com/rss.xml"
+	    },
+	    {
+	        "idnum": "11",
+	        "name": "Jalopnik",
+	        "url": "http://www.jalopnik.com",
+	        "rss": "http://feeds.gawker.com/jalopnik/full"
+	    },
+	    {
+	        "idnum": "12",
+	        "name": "David Walsh :: Web Dev Tutorials",
+	        "url": "http://davidwalsh.name/",
+	        "rss": "http://davidwalsh.name/feed"
+	    },
+	    {
+	        "idnum": "13",
+	        "name": "DZine Blog",
+	        "url": "http://dzineblog.com/",
+	        "rss": "http://feeds2.feedburner.com/WebWorldST"
+	    },
+	    {
+	        "idnum": "15",
+	        "name": "Scriptiny",
+	        "url": "http://www.scriptiny.com/",
+	        "rss": "http://feeds.feedburner.com/leigeber"
+	    },
+	    {
+	        "idnum": "16",
+	        "name": "PSD Tuts+",
+	        "url": "http://psd.tutsplus.com/",
+	        "rss": "http://feeds.feedburner.com/psdtuts"
+	    },
+	    {
+	        "idnum": "17",
+	        "name": "Photoshop Star",
+	        "url": "http://www.photoshopstar.com/",
+	        "rss": "http://feeds.feedburner.com/photoshopstarcom"
+	    },
+	    {
+	        "idnum": "18",
+	        "name": "Smashing Magazine",
+	        "url": "http://www.smashingmagazine.com/",
+	        "rss": "http://rss1.smashingmagazine.com/feed/"
+	    },
+	    {
+	        "idnum": "22",
+	        "name": "Polygon",
+	        "url": "http://www.polygon.com",
+	        "rss": "http://www.polygon.com/rss/index.xml"
+	    },
+	    {
+	        "idnum": "21",
+	        "name": "Kotaku",
+	        "url": "http://kotaku.com/",
+	        "rss": "http://kotaku.com/vip.xml"
+	    },
+	    {
+	        "idnum": "26",
+	        "name": "Local Austin News",
+	        "url": "http://www.kvue.com",
+	        "rss": "http://rssfeeds.kvue.com/kvue/home"
+	    },
+	    {
+	        "idnum": "24",
+	        "name": "Central Ohio News",
+	        "url": "http://www.10tv.com/",
+	        "rss": "http://www.10tv.com/content/syndication/news_central-ohio.xml"
+	    },
+	    {
+	        "idnum": "25",
+	        "name": "Tested.com",
+	        "url": "http://www.tested.com/",
+	        "rss": "http://www.tested.com/feeds/"
+	    },
+	    {
+	        "idnum": "27",
+	        "name": "BlizzardWatch",
+	        "url": "http://blizzardwatch.com/",
+	        "rss": "http://feeds.feedburner.com/BlizzardWatch"
+	    }
+	]
+};
